aa8214b23559d3c3b4c7e1fdefc7f0abb750fc08
dmitry <dmitry.kouznetsov@paxport.se>
2016-06-15 22:16:38 +0200

    leaderboard

--- imports/ui/components/leaderboard.js
+++ imports/ui/components/leaderboard.js
@@ -4,3 +4,41 @@ import { Template } from 'meteor/templating'
 import { Leaderboards } from '../../api/leaderboards.js'
 
 import './leaderboard.html'
+
+Template.App_leaderboard.onCreated(function() {
+  this.subscribe('leaderboards')
+})
+
+Template.App_leaderboard.helpers({
+  users() {
+    return Leaderboards.findOne().users
+  },
+  flagImageUrl(team) {
+    return "/images/flags/" + team.toLowerCase() + ".png";
+  },
+  lastMatch() {
+    return Leaderboards.findOne().lastMatch
+  },
+  lastMatchTeamsAndScore() {
+    const lastMatch = Leaderboards.findOne().lastMatch
+    return ''
+    + lastMatch.team1
+    + ' '
+    + lastMatch.result[lastMatch.team1]
+    + ' - '
+    + lastMatch.result[lastMatch.team2]
+    + ' '
+    + lastMatch.team2
+  },
+  deltaStyle(delta) {
+    if (delta === 3) {
+      return "background-color: #66FF99"
+    } else if (delta === 1) {
+      return "background-color: #FFFF99"
+    } else if (delta === 0) {
+      return "background-color: #FF9999"
+    } else {
+      return "background-color: #C8C8C8"
+    }
+  }
+})
